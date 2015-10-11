05af81428fe5a5ab2279b943531793911f5ec02e
Claudio Benghi <claudio.benghi@gmail.com>
2015-10-12 00:06:13 +0100

    Smaller changes.

--- www/Bird.php
+++ www/Bird.php
@@ -28,15 +28,15 @@ include_once("bird_array.php");
 				<img style="width: 100%;" src="/img/birdQuestionMark.png">
 			</div>
 			<div id="answer">
-				<p>It was the <b><?= $BirdNames[$BirdID] ?>!</p>
+				<p>It was the <b><?= $BirdNames[$BirdID] ?></b>!</p>
 				<img style="width: 100%;" src="/img/<?= $BirdID ?>.jpg">
-				<p><b>Did you know that birds are in need of help?</b></p>
+				<p><b>Do you know that birds are in need of help?</b></p>
 				<p>Their numbers are in the decline also because a decrease in suitable nesting sites. 
 				This means that building a birdhouse/nest box for birds will not only entice them to stay in your yard, but help these birds chance for survival.</p>
 				<p><a href="/">Try again</a></p>
 			</div>
 			<div id="allbusy">
-				<p>Sorrry, all speakers are busy!</p>
+				<p>Sorry, all speakers are busy!</p>
 				<p><a href="/">Try again</a> in a little-bit</p>
 			</div>
         </div>
